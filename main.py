8d09a2ba838b9009f86751ae72f0e1ce0d08afc5
iamniama <plectrumonthespectrum@gmail.com>
2022-02-25 13:20:34 -0700

    Minor tweaks for testing

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ import pprint
 if __name__ == "__main__":
     cup = Roll()
     pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(cup.roll_dice(input()))
+    roll_info = cup.roll_dice(input())
+    pp.pprint(roll_info)
+    print(f"Your total roll was: {roll_info['dice_info']['roll_result']}")
